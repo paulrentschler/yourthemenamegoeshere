6a1849b01e98c582d4c1fb0847f20ba7453ca2bc
Craig Haynal <cah190@psu.edu>
2011-06-07 16:43:32 -0400

    Fix typo.

--- transform.sh
+++ transform.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 SHORTNAME=awesome
 FULLNAME="Awesome Theme"
-find -X plonetheme.yournamegoeshere | xargs -I {} \
-sed 's:yournamegoeshere:'$SHORTNAME':g;s:Your Theme Name Goes Here:'$FULLNAME':g' \
+find -X plonetheme.yourthemenamegoeshere | xargs -I {} \
+sed 's:yourthemenamegoeshere:'$SHORTNAME':g;s:Your Theme Name Goes Here:'$FULLNAME':g' \
     < {} > {}
-mv plonetheme.yournamegoeshere/plonetheme/yournamegoeshere/stylesheets/yournamegoeshere.css plonetheme.$SHORTNAME/plonetheme/$SHORTNAME/stylesheets/$SHORTNAME.css
-mv plonetheme.yournamegoeshere/plonetheme/yournamegoeshere plonetheme.$SHORTNAME/plonetheme/$SHORTNAME
-mv plonetheme.yournamegoeshere plonetheme.$SHORTNAME
+mv plonetheme.yourthemenamegoeshere/plonetheme/yourthemenamegoeshere/stylesheets/yourthemenamegoeshere.css plonetheme.$SHORTNAME/plonetheme/$SHORTNAME/stylesheets/$SHORTNAME.css
+mv plonetheme.yourthemenamegoeshere/plonetheme/yourthemenamegoeshere plonetheme.$SHORTNAME/plonetheme/$SHORTNAME
+mv plonetheme.yourthemenamegoeshere plonetheme.$SHORTNAME
 
 
 
