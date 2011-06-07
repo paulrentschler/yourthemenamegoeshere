d44f6e4a08e71a2a7e3a2ee27e0c5993fbe60ca5
Craig Haynal <cah190@psu.edu>
2011-06-07 16:24:53 -0400

    Uninstall code, some default (disabled) css.

--- plonetheme.yourthemenamegoeshere/plonetheme/yourthemenamegoeshere/Extensions/Install.py
+++ plonetheme.yourthemenamegoeshere/plonetheme/yourthemenamegoeshere/Extensions/Install.py
@@ -1,5 +1,27 @@
 from Products.CMFCore.utils import getToolByName
 
+THEMENAME='Your Theme Name Goes Here'
+
 def uninstall(portal):
     skins_tool = getToolByName(portal, 'portal_skins')
-    import pdb; pdb.set_trace()
+
+    # Try to clean up
+    try:
+        del skins_tool['selections'][THEMENAME]
+    except:
+        pass
+
+    # Set a reasonable default skin
+    if skins_tool.getDefaultSkin() == THEMENAME:
+        skins = skins_tool.getSkinSelections()
+        default_skins = ['Sunburst Theme', 'Plone Classic Theme', 'Plone Default']
+        for ds in default_skins:
+            if ds in skins:
+                skins_tool.default_skin = ds
+                break
+        if skins_tool.getDefaultSkin() == THEMENAME:
+            return "%s uninstalled, but unable to set a default skin." % THEMENAME
+
+        return "%s uninstalled; default skin set to %s." % (THEMENAME, ds)
+    return "%s uninstalled." % THEMENAME
+
